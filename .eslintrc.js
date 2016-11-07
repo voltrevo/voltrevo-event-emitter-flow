dce617858d59b543b3adfbaab07887614b5b6bc9
Andrew Morris <voltrevo@gmail.com>
2016-11-07 11:42:39 +1100

    Config setup

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "parser": "babel-eslint",
+    "installedESLint": true,
+    "plugins": [
+        "import",
+        "flowtype"
+    ],
+    "rules": {
+        "new-cap": 0
+    }
+};
